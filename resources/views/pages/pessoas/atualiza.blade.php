f9698f2244366279d9df8396560b96054cb74cf5
Zaetron <79610098+FrodoMenezes@users.noreply.github.com>
2023-10-09 10:21:03 -0300

    Update atualiza.blade.php

--- resources/views/pages/pessoas/atualiza.blade.php
+++ resources/views/pages/pessoas/atualiza.blade.php
@@ -2,27 +2,55 @@
 
 @section('content')
 
-<form class="form" method="POST" action="{{ route('atualizar.lancamento', $findLancamento->id) }}" >
+<form class="form" method="POST" action="{{ route('atualizar.pessoas', $findPessoa->id) }}" >
     
     @csrf
     @method('PUT')
 
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Atualizar lançamento</h1>
+        <h1 class="h2">Atualizar Pessoa</h1>
     </div>
 
     <div class="mb-3">
         <label class="form-label">Nome</label>
-        <input type="text" value=" {{ isset($findLancamento->nome) ? $findLancamento->nome : old('nome') }} " class="form-control" @error('nome') is-invalid @enderror name="nome">
+        <input type="text" value=" {{ isset($findPessoa->nome) ? $findPessoa->nome : old('nome') }} " class="form-control" @error('nome') is-invalid @enderror name="nome">
         @if ($errors->has('nome'))
             <div class="invalid-feedback"> {{ $errors->first('nome') }} </div>
         @endif
     </div>
     <div class="mb-3">
-        <label class="form-label">Valor</label>
-        <input {{-- id="marcara_valor" --}} value=" {{ isset($findLancamento->valor) ? $findLancamento->valor : old('valor') }} " class="form-control" @error('valor') is-invalid @enderror name="valor">
-        @if ($errors->has('valor'))
-            <div class="invalid-feedback"> {{ $errors->first('valor') }} </div>
+        <label class="form-label">E-mail</label>
+        <input value=" {{ isset($findPessoa->email) ? $findPessoa->email : old('email') }} " class="form-control" @error('email') is-invalid @enderror name="email">
+        @if ($errors->has('email'))
+            <div class="invalid-feedback"> {{ $errors->first('email') }} </div>
+        @endif
+    </div>
+    <div class="mb-3">
+        <label class="form-label">CEP</label>
+        <input id="cep" value=" {{ isset($findPessoa->cep) ? $findPessoa->cep : old('cep') }} " class="form-control" @error('cep') is-invalid @enderror name="cep">
+        @if ($errors->has('cep'))
+            <div class="invalid-feedback"> {{ $errors->first('cep') }} </div>
+        @endif
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Endereço</label>
+        <input id="endereco" value=" {{ isset($findPessoa->endereco) ? $findPessoa->endereco : old('endereco') }} " class="form-control" @error('endereco') is-invalid @enderror name="endereco">
+        @if ($errors->has('endereco'))
+            <div class="invalid-feedback"> {{ $errors->first('endereco') }} </div>
+        @endif
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Logradouro</label>
+        <input id="logradouro" value=" {{ isset($findPessoa->logradouro) ? $findPessoa->logradouro : old('logradouro') }} " class="form-control" @error('logradouro') is-invalid @enderror name="logradouro">
+        @if ($errors->has('logradouro'))
+            <div class="invalid-feedback"> {{ $errors->first('logradouro') }} </div>
+        @endif
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Bairro</label>
+        <input id="bairro" value=" {{ isset($findPessoa->bairro) ? $findPessoa->bairro : old('bairro') }} " class="form-control" @error('bairro') is-invalid @enderror name="bairro">
+        @if ($errors->has('bairro'))
+            <div class="invalid-feedback"> {{ $errors->first('bairro') }} </div>
         @endif
     </div>
 
